1c7c8a0b760bedf1c018e47fc3a9e3d07b7d8598
Isuru Kalhara <isurukalharag@gmail.com>
2025-01-13 00:41:20 +0530

    Working prototype

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -11,6 +11,7 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
+    public Text BestScoreText;
     public GameObject GameOverText;
     
     private bool m_Started = false;
@@ -22,6 +23,9 @@ public class MainManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (DataHandler.Instance != null){
+            BestScoreText.text = "Best Score : "+DataHandler.Instance.bestPlayerName + " : " + DataHandler.Instance.bestPlayerScore;  
+        }
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
         
@@ -57,7 +61,9 @@ public class MainManager : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                DataHandler.Instance.playerScore = m_Points;
+                DataHandler.Instance.SavePlayer();
+                SceneManager.LoadScene(0);
             }
         }
     }
